8fe0f17eddd79611001f5cec4b514c3a2944e442
Preston Whitehall <pmwhitehall@gmail.com>
2019-09-26 14:47:08 -0700

    Sets db url to external url config file

--- database/database.js
+++ database/database.js
@@ -1,7 +1,8 @@
 const { Pool } = require('pg');
+const url = require('./urls');
 
 const pool = new Pool({
-  host: 'localhost',
+  host: url.host,
   database: 'menus',
   port: 5432,
 });
---
+++ database/urls.js
@@ -0,0 +1,9 @@
+const host = '';
+const menu = '';
+const reservations = '';
+const reviews = '';
+const photos = '';
+
+module.exports = {
+  host, menu, reservations, reviews, photos,
+};
